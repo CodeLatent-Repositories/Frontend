179aef1bd3f86aab8cd60d7f8d3cb4030f66eace
Mehul Mohan <mehulmpt@gmail.com>
2025-01-07 22:53:45 +0530

    chore: cleanup

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  assetPrefix: 'https://custom-webpage-static-assets-production.fermion.app/6758087080313337cad7883a-custom-zip-pages/',
   images: {
     remotePatterns: [
       {
